d4488fef20473b01cdb716c3e26b296818642738
David Pimentel <davidp@foursquare.com>
2015-10-11 21:23:48 -0700

    press enter to open bookmark

--- js/browser_action.js
+++ js/browser_action.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   var bookmarks;
+  var filteredBookmarks;
+  var activeBookmarkIndex = 1;
   var fuse;
 
   chrome.storage.local.get("bookmarks", function(result) {
@@ -21,23 +23,39 @@ $(document).ready(function() {
     var input = $('#searchBar');
     var results = $('#results');
 
-    var keyup = Rx.Observable.fromEvent(input, 'keyup')
+    var keyupObservable = Rx.Observable.fromEvent(input, 'keyup')
       .map(function (e) {
         return e.target.value;
       })
-      .debounce(750)
+      .debounce(250)
       .distinctUntilChanged();
 
-    var searcher = keyup.flatMapLatest(fuzzySearch);
+    var commandKeypress = Rx.Observable.fromEvent(input, 'keydown')
+      .filter(isCommandKeypress)
+      .do(function(e) { e.preventDefault();})
+      .map(function (e) {
+        switch(e.keyCode) {
+          case 38:
+            return "up";
+          case 40:
+            return "down";
+          case 13:
+            return "enter";
+        }
+      });
 
-    var subscription = searcher.subscribe(
+    var searcher = keyupObservable.flatMapLatest(fuzzySearch);
+
+    var keyupSubscription = searcher.subscribe(
       function (data) {
-
+        activeBookmarkIndex = 1;
+        filteredBookmarks = data;
         results.empty();
 
         $.each(data, function(i, value) {
           $('<li>' + value.title + '</li>').appendTo(results);
         });
+        highlightSelectedBookmark();
       },
       function (error) {
         // Handle any errors
@@ -45,6 +63,39 @@ $(document).ready(function() {
 
         $('<li>Error: ' + error + '</li>').appendTo(results);
     });
+
+    var commandSubscription = commandKeypress.subscribe(
+      function(keypress) {
+        switch(keypress) {
+          case "up":
+            activeBookmarkIndex = activeBookmarkIndex > 1 ? activeBookmarkIndex - 1 : filteredBookmarks.length;
+            highlightSelectedBookmark();
+            break;
+          case "down":
+            activeBookmarkIndex = activeBookmarkIndex < filteredBookmarks.length ?
+              activeBookmarkIndex + 1 : 1;
+            highlightSelectedBookmark();
+            break;
+          case "enter":
+            var id = filteredBookmarks[activeBookmarkIndex - 1].id;
+            chrome.bookmarks.get(id, function(bookmarks) {
+              if (bookmarks.length > 0) {
+                var url = bookmarks[0].url;
+                chrome.tabs.create({url: url}, function(){});
+              }
+            });
+            break;
+        }
+
+      });
   }
 
-})
+  function isCommandKeypress(e) {
+    return e.keyCode == 38 || e.keyCode == 40 || e.keyCode == 13;
+  }
+  function highlightSelectedBookmark() {
+    $("#results > li").removeClass("active");
+    $("#results > li:nth-child(" + activeBookmarkIndex + ")").addClass("active");
+  }
+
+});
